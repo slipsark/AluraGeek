e33e482f20ca01fc89b33552800ef26ecda7a6f5
Edwin Cevallos <edwin_paul01@hotmail.com>
2023-05-12 21:40:13 -0500

    Proyecto en cero reseteado

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+const menushowClick = document.querySelector(".toggle");
+const menushow = document.querySelector(".nav");
+
+menushowClick.addEventListener("click", ()=> {
+  menushow.classList.toggle("show")
+})
+
+
+console.log("Pruba de coneción");
