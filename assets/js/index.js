a1bc74325d1db5b12c59220a32e1c3714d632827
Edwin Cevallos <edwin_paul01@hotmail.com>
2023-05-24 22:28:27 -0500

    Formulario de login funcional

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,18 @@ const searchInput = document.querySelector(".search__descripcion");
 searchClick.addEventListener("click", ()=> {
   searchInput.classList.toggle("search__show")
 })
+
+/* Validació en acceso */
+const usuario = document.querySelector(".user");
+const pass = document.querySelector(".password");
+const formAction = document.querySelector(".submit");
+const alerta = document.querySelector(".alertas");
+
+formAction.addEventListener("submit", (e)=> {
+  e.preventDefault();
+  const user = usuario.value;
+  const passw = pass.value;
+
+  user === "admin" && passw === "admin" ? window.location = "../Inventario/index.html" :
+    alerta.innerHTML = "El usuario o la contraseña son incorrectos";
+})
